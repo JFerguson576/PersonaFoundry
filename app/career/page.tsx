7eedf383c24ef1818c8da380053446f8713a1c42
John Ferguson <Johnliamferguson@gmail.com>
2026-04-18 08:42:32 +1200

    Force dynamic rendering for career page to avoid prerender error

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -1,5 +1,7 @@
 import { CareerHomeClient } from "@/components/career/CareerHomeClient"
 
+export const dynamic = "force-dynamic"
+
 export default function CareerPage() {
   return <CareerHomeClient />
 }
